7bf8b807ed1fa3f8b5febab3119706e2ffdb0147
Neamul01 <neamulhaque2002@gmail.com>
2022-04-11 14:17:39 +0600

    components created

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><small>Copywrite &copy; Noman</small></p>
+        </footer>
+    );
+};
+
+export default Footer;
