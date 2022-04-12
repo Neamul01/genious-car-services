8fe3044aa41cb44a841f60ed7c2a4abe5c9e6d34
Neamul01 <neamulhaque2002@gmail.com>
2022-04-12 20:17:32 +0600

    log added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from '../../../images/logo.png'
 
 const Header = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand as={Link} to='/'>Navbar</Navbar.Brand>
-                <Nav className="me-auto">
+                <Navbar.Brand as={Link} to='/'><img style={{
+                    width: '12rem'
+                }} src={logo} alt="" /></Navbar.Brand>
+                <Nav className="ms-auto">
                     <Nav.Link as={Link} to='/'>Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link as={Link} to='/about'>About</Nav.Link>
