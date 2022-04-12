d5b6b25af20afaa52bb77e5375711465950af3de
Neamul01 <neamulhaque2002@gmail.com>
2022-04-12 18:05:04 +0600

    nav bar edited

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                <Navbar.Brand as={Link} to='/'>Navbar</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to='/'>Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link as={Link} to='/about'>About</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
