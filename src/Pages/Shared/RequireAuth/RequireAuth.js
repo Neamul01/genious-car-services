8e729d5dc1b0789669cb7d1a7d826ff169882d5f
Neamul01 <neamulhaque2002@gmail.com>
2022-04-22 22:06:36 +0600

    email verificarion and some other change

--- src/Pages/Shared/RequireAuth/RequireAuth.js
+++ src/Pages/Shared/RequireAuth/RequireAuth.js
@@ -1,11 +1,19 @@
+import { sendEmailVerification } from 'firebase/auth';
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import useUser from '../../../hooks/useUser';
 
 
 const RequireAuth = ({ children }) => {
     const { authUser, loading } = useUser();
     const location = useLocation();
+
+    const handleEmailVarification = async () => {
+        await sendEmailVerification(auth.currentUser).then(() => {
+            alert('mail sent')
+        })
+    }
 
     if (loading) {
         return <p>Loading...</p>
@@ -14,6 +22,15 @@ const RequireAuth = ({ children }) => {
     if (!authUser.accessToken) {
         return <Navigate to={'/login'} state={{ from: location }} replace />
     }
+
+    if (!authUser.emailVerified) {
+        return <div>
+            <h2 className="text-danger">Your Email is not varified</h2>
+            <h5 className="text-success">Please varify your Email Address</h5>
+            <button onClick={handleEmailVarification}>Send Again</button>
+        </div>
+    }
+
     return children;
 };
 
