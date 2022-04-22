ca41e811a463c02caf83da2d6e590a52fbc30658
Neamul01 <neamulhaque2002@gmail.com>
2022-04-23 02:49:09 +0600

    manage service

--- src/Pages/ManageService/ManageService.js
+++ src/Pages/ManageService/ManageService.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import useServices from '../../hooks/useServices';
 
 const ManageService = () => {
+    const [services, setServices] = useServices();
+    // console.log(services)
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you want to delete?');
+        if (proceed) {
+            fetch(`http://localhost:5000/service/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(result => {
+                    console.log(result)
+                    const remaining = services.filter(service => service._id !== id);
+                    setServices(remaining)
+                })
+        }
+        // console.log(id)
+    }
+
     return (
-        <div>
-            <h2>Manage Your Services</h2>
-        </div>
+        <div className='w-50 mx-auto'>
+            <h2 className='mb-4'>Manage Your Services</h2>
+            {
+                services?.map(service => <div key={service._id}>
+                    <h5>{service.name} <button onClick={() => handleDelete(service._id)}>X</button> </h5>
+                </div>)
+            }
+        </div >
     );
 };
 
